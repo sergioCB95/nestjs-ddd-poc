f6f3348a6897a9c26276565de8cabf1de701bd5d
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2023-07-06 15:29:18 +0200

    refactor: remove app components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommonsModule } from './commons/commons.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [CommonsModule, OrderModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
