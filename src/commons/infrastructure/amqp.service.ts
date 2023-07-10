ceaf0de3713a71bf4a317297c456b9c1b82256cb
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2023-07-10 18:47:01 +0200

    using rascal library for rabbitMQ integration

--- src/commons/infrastructure/amqp.service.ts
+++ src/commons/infrastructure/amqp.service.ts
@@ -1,34 +1,38 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import amqp, { ChannelWrapper } from 'amqp-connection-manager';
+import { BrokerAsPromised as Broker } from 'rascal';
 
 @Injectable()
 export class AmqpService implements OnModuleInit {
-  private channelWrapper: ChannelWrapper;
-  private queues = ['nestjs-ddd-poc-queue'];
-  private amqpUrl = 'amqp://localhost:5672';
-
-  private createMQProducer(amqpUrl: string, queues: string[]) {
-    const connection = amqp.connect(amqpUrl);
-    connection.on('connect', () => console.log('Connected!'));
-    connection.on('connectFailed', (err) =>
-      console.log('Connection failed.', err),
-    );
-    connection.on('disconnect', (err) => console.log('Disconnected.', err));
-    this.channelWrapper = connection.createChannel({
-      json: true,
-      setup: (channel) => {
-        queues.forEach((queue) => {
-          channel.assertQueue(queue, { durable: true });
-        });
+  private broker: Broker;
+  private async createMQProducer() {
+    this.broker = await Broker.create({
+      vhosts: {
+        '/': {
+          connection: {
+            url: 'amqp://guest:guest@localhost:5672/',
+          },
+          exchanges: ['nestjs-ddd-poc-exchange'],
+          queues: ['nestjs-ddd-poc-queue'],
+          bindings: [
+            'nestjs-ddd-poc-exchange[nestjs-ddd-poc.v1.order.created] -> nestjs-ddd-poc-queue',
+          ],
+          publications: {
+            order_created: {
+              exchange: 'nestjs-ddd-poc-exchange',
+              routingKey: 'nestjs-ddd-poc.v1.order.created',
+            },
+          },
+        },
       },
     });
+    this.broker.on('error', console.error);
   }
 
   async onModuleInit() {
-    await this.createMQProducer(this.amqpUrl, this.queues);
+    await this.createMQProducer();
   }
 
-  async publish(queue: string, message: any) {
-    return this.channelWrapper.sendToQueue(queue, message);
+  async publish(event: string, message: any) {
+    return this.broker.publish(event, message);
   }
 }
