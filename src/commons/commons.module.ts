19bd29273ef2258aad1c284142c80b0ea88b1608
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2023-07-20 15:20:48 +0200

    fixing commons module

--- src/commons/commons.module.ts
+++ src/commons/commons.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './infrastructure/prisma.service';
 import { AmqpService } from './infrastructure/amqp.service';
-import { EventBus } from './domain/event.bus';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [PrismaService, AmqpService, EventBus],
-  exports: [PrismaService, AmqpService, EventBus],
+  providers: [PrismaService, AmqpService],
+  exports: [PrismaService, AmqpService],
 })
 export class CommonsModule {}
